7916eb3f17a2b008e47bbacbb44545c5b14893d9
Nick <neverate@gmail.com>
2020-02-11 14:00:33 +0200

    + hooked up eslint, set up the linting rules

--- v1/server/src/index.ts
+++ v1/server/src/index.ts
@@ -1,23 +1,23 @@
 import express from "express";
 
 const app = express();
-const port: number = 8888;
+const port = 8888;
 
-const greeting: string = "Howdy pilgrim!";
+const greeting = "Howdy pilgrim!";
 
 app.get("/", (req, res) => {
   console.log(req.ip);
   res.send(greeting);
 });
 
-app.get("/norm", (req, res) => {
+app.get("/norm", (_req, res) => {
   res.send("I like bananas. They're yellow.");
 });
 
-let a: number = 5,
-  b: number = 7;
+const a = 5,
+  b = 7;
 
-app.get("/math", (req, res) => {
+app.get("/math", (_req, res) => {
   res.send(`${a} + ${b} = ${a + b}`);
 });
 
